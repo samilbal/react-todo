16e16d194a72dba19aa43c797fc501fa4e8ab6e4
Samil <policijska@gmail.com>
2022-07-18 16:23:13 +0300

    Added functions 'toggle' and 'delete'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,61 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState, useReducer } from "react";
+import Todo from "./Todo";
+
+export const ACTIONS = {
+  ADD_TODO: "ADD_TODO",
+  TOGGLE_TODO: "TOGGLE_TODO",
+  DELETE_TODO: "DELETE_TODO",
+};
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.title)];
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete };
+        }
+        return todo;
+      });
+    case ACTIONS.DELETE_TODO:
+      return todos.filter((todo) => todo.id !== action.payload.id);
+
+    default:
+      return todos;
+  }
+}
+
+function newTodo(title) {
+  return { id: Date.now(), title: title, complete: false };
+}
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { title: title } });
+    setTitle("");
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+      </form>
+      {todos.map((todo) => {
+        return <Todo key={todo.id} todo={todo} dispatch={dispatch} />;
+      })}
+    </div>
+  );
 }
 
 export default App;
