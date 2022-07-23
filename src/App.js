7a5bbf7987bb5e118f7dc695c6e06c89bf53332f
Samil <policijska@gmail.com>
2022-07-23 13:42:22 +0300

    Styling etc.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
 import { useState, useReducer } from "react";
-import Todo from "./Todo";
-
-export const ACTIONS = {
-  ADD_TODO: "ADD_TODO",
-  TOGGLE_TODO: "TOGGLE_TODO",
-  DELETE_TODO: "DELETE_TODO",
-};
-
-function reducer(todos, action) {
-  switch (action.type) {
-    case ACTIONS.ADD_TODO:
-      return [...todos, newTodo(action.payload.title)];
-    case ACTIONS.TOGGLE_TODO:
-      return todos.map((todo) => {
-        if (todo.id === action.payload.id) {
-          return { ...todo, complete: !todo.complete };
-        }
-        return todo;
-      });
-    case ACTIONS.DELETE_TODO:
-      return todos.filter((todo) => todo.id !== action.payload.id);
-
-    default:
-      return todos;
-  }
-}
-
-function newTodo(title) {
-  return { id: Date.now(), title: title, complete: false };
-}
+import Todo from "./components/Todo";
+import { ACTIONS } from "./components/ACTIONS";
+import reducer from "./components/Reducer";
 
 function App() {
   const [todos, dispatch] = useReducer(reducer, []);
@@ -42,6 +15,7 @@ function App() {
 
   return (
     <div className="App">
+      <h1>To-Do App</h1>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -50,6 +24,7 @@ function App() {
             setTitle(e.target.value);
           }}
         />
+        <button type="submit">Add</button>
       </form>
       {todos.map((todo) => {
         return <Todo key={todo.id} todo={todo} dispatch={dispatch} />;
